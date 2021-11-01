ac45c2f4504301893a4d36f99b97e3dff7fc3d3a
Hernán Massad <hernan.massad@gmail.com>
2021-11-01 11:02:45 -0300

    update next.js version

--- components/Typography.stories.tsx
+++ components/Typography.stories.tsx
@@ -7,9 +7,15 @@ export default {
 
 export const Default = () => (
   <div>
-    <div>{text}</div>
     <div className="font-hand">{text}</div>
+    <div className="font-thin">{text}</div>
+    <div className="font-extralight">{text}</div>
+    <div className="font-light">{text}</div>
+    <div className="font-normal">{text}</div>
+    <div className="font-medium">{text}</div>
+    <div className="font-semibold">{text}</div>
     <div className="font-bold">{text}</div>
+    <div className="font-extrabold">{text}</div>
     <div className="font-black">{text}</div>
   </div>
 );
