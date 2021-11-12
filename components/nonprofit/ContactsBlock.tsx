3f613744b65b2b91d85176016c8a70fc43071919
eschur <eric.vp.94@gmail.com>
2021-11-11 23:12:37 -0300

    ContactsBlock

--- components/nonprofit/ContactsBlock.tsx
+++ components/nonprofit/ContactsBlock.tsx
@@ -1,15 +1,20 @@
 import { Block, EmptyBlock } from "./Block";
-import { ContactInfoIcon } from "./Icons";
+import { ContactInfoIcon, EditPenIcon } from "./Icons";
 import React, { useState } from "react";
 import Nonprofit from "../../dtos/Nonprofit";
 import Modal from "../Modal";
 import Button from "../Button";
-
-const ContactsBlock = ({nonprofit, ownsNonprofit}: {nonprofit: Nonprofit, ownsNonprofit: boolean}) => {
+import { FaMailBulk, FaPhone } from "react-icons/fa";
+const ContactsBlock = ({
+  nonprofit,
+  ownsNonprofit,
+}: {
+  nonprofit: Nonprofit;
+  ownsNonprofit: boolean;
+}) => {
   const [isEditorOpen, setEditorOpen] = useState(false);
 
-  const handleSave = (
-  ) => {
+  const handleSave = () => {
     try {
       // todo save to db
     } catch (e) {
@@ -18,44 +23,78 @@ const ContactsBlock = ({nonprofit, ownsNonprofit}: {nonprofit: Nonprofit, ownsNo
     }
   };
 
-  if (!nonprofit?.contacts?.length) {
-    if (!ownsNonprofit) {
+  if (ownsNonprofit) {
+    return (
+      <div className="flex flex-col">
+        {nonprofit?.contacts?.length &&
+          nonprofit.contacts.map((contact) => {
+            <Block>
+              <div className="flex">
+                <div className="font-bold text-16px leading-30px t:text-20px">
+                  {contact.branch}
+                </div>
+                <div
+                  onClick={() => setEditorOpen(true)}
+                  className="cursor-pointer ml-5px"
+                >
+                  <EditPenIcon />
+                </div>
+              </div>
+              <div className="flex flex-col font-light text-13px leading-19-5px t:text-14px t:leading-21px">
+                <FaMailBulk />
+                <span className="underline">{contact.primaryEmailAddress}</span>
+              </div>{" "}
+              <div className="flex flex-col font-light text-13px leading-19-5px t:text-14px t:leading-21px">
+                <FaPhone />
+                <span>{contact.primaryPhoneNumber}</span>
+              </div>{" "}
+              <div className="flex flex-col font-light text-13px leading-19-5px t:text-14px t:leading-21px">
+                <span>{contact.website}</span>
+              </div>
+            </Block>;
+          })}
+        <EmptyBlock
+          icon={<ContactInfoIcon />}
+          text={
+            <>
+              <span className="font-bold">Add contact info</span>
+              <br />
+              and make it easy to find out more about your cause.
+            </>
+          }
+          actionText="Add contacts"
+          onClick={() => setEditorOpen(true)}
+        />
+      </div>
+    );
+  } else {
+    if (nonprofit?.contacts?.length) {
+      {
+        nonprofit.contacts.map((contact) => {
+          <Block>
+            <div className="flex">
+              <div className="font-bold text-16px leading-30px t:text-20px">
+                {contact.branch}
+              </div>
+            </div>
+            <div className="flex flex-col font-light text-13px leading-19-5px t:text-14px t:leading-21px">
+              <FaMailBulk />
+              <span className="underline">{contact.primaryEmailAddress}</span>
+            </div>{" "}
+            <div className="flex flex-col font-light text-13px leading-19-5px t:text-14px t:leading-21px">
+              <FaPhone />
+              <span>{contact.primaryPhoneNumber}</span>
+            </div>{" "}
+            <div className="flex flex-col font-light text-13px leading-19-5px t:text-14px t:leading-21px">
+              <span>{contact.website}</span>
+            </div>
+          </Block>;
+        });
+      }
+    } else {
       return null;
     }
-
-    return (
-      <EmptyBlock
-        icon={<ContactInfoIcon />}
-        text={
-          <>
-            <span className="font-bold">Add contact info</span>
-            <br />
-            and make it easy to find out more about your cause.
-          </>
-        }
-        actionText="Add contacts"
-        onClick={() => setEditorOpen(true)}
-      />
-    );
   }
-
-  return (
-    <Block>
-      CONTACTS
-
-      {isEditorOpen && (
-        <Modal
-          header="Update your testimonials"
-          onClose={() => setEditorOpen(false)}
-          footer={<Button onClick={handleSave}>Save</Button>}
-        >
-          <div className="flex flex-col p-20px">
-            CONTACTS EDITOR
-          </div>
-        </Modal>
-      )}
-    </Block>
-  );
 };
 
 export default ContactsBlock;
--- components/nonprofit/NonprofitProfileTabs.tsx
+++ components/nonprofit/NonprofitProfileTabs.tsx
@@ -7,29 +7,44 @@ import TestimonialsBlock from "./TestimonialsBlock";
 import CampaignsBlock from "./CampaignsBlock";
 import ContactsBlock from "./ContactsBlock";
 import ClosedCampaignsBlock from "./ClosedCampaignsBlock";
+import { EmptyBlock } from "./Block";
+import { ContactInfoIcon } from "./Icons";
 
-type Tab = "home" | "our impact" | "campaigns" | "fundraiser" | "posts" | "contact";
+type Tab =
+  | "home"
+  | "our impact"
+  | "campaigns"
+  | "fundraiser"
+  | "posts"
+  | "contact";
 
 const NonprofitProfileTabs: FC<{
   nonprofit: Nonprofit;
-  ownsNonprofit: boolean,
-  className?: string,
-}> = ({
-  nonprofit,
-  ownsNonprofit,
-  className = "",
-}) => {
+  ownsNonprofit: boolean;
+  className?: string;
+}> = ({ nonprofit, ownsNonprofit, className = "" }) => {
   const [selected, setSelected] = useState<Tab>("home");
 
   return (
     <div className={`w-full flex flex-col ${className}`}>
       <div className="overflow-x-auto mb-30px">
         <div className="text-center flex gap-30px text-14px leading-16px font-light select-none">
-          {["home", "our impact", "campaigns", /*"fundraiser", "posts",*/ "contact"].map((tab: Tab) => (
+          {[
+            "home",
+            "our impact",
+            "campaigns",
+            /*"fundraiser", "posts",*/ "contact",
+          ].map((tab: Tab) => (
             <div
               key={tab}
-              className={`border-b-3px py-10px px-5px ${selected === tab ? "border-primary font-bold" : "border-transparent"} cursor-pointer uppercase whitespace-nowrap`}
-              onClick={() => {setSelected(tab)}}
+              className={`border-b-3px py-10px px-5px ${
+                selected === tab
+                  ? "border-primary font-bold"
+                  : "border-transparent"
+              } cursor-pointer uppercase whitespace-nowrap`}
+              onClick={() => {
+                setSelected(tab);
+              }}
             >
               {tab}
             </div>
@@ -38,31 +53,52 @@ const NonprofitProfileTabs: FC<{
       </div>
 
       {selected === "home" && (
-        <div
-          className="w-full m-0 p-0 min-w-280px flex flex-col t:flex-row t:flex-wrap items-center gap-29px"
-        >
+        <div className="w-full m-0 p-0 min-w-280px flex flex-col t:flex-row t:flex-wrap items-center gap-29px">
           <MasonryList>
             <AboutUsBlock nonprofit={nonprofit} ownsNonprofit={ownsNonprofit} />
-            <GalleryBlock nonprofit={nonprofit} ownsNonprofit={ownsNonprofit} />
-            <CampaignsBlock nonprofit={nonprofit} ownsNonprofit={ownsNonprofit} />
-            <ContactsBlock nonprofit={nonprofit} ownsNonprofit={ownsNonprofit} />
-            <TestimonialsBlock nonprofit={nonprofit} ownsNonprofit={ownsNonprofit} />
+            <GalleryBlock
+              nonprofit={nonprofit}
+              ownsNonprofit={ownsNonprofit}
+              onEditClick={() => {}}
+            />
+            <CampaignsBlock
+              nonprofit={nonprofit}
+              ownsNonprofit={ownsNonprofit}
+            />
+            <ContactsBlock
+              nonprofit={nonprofit}
+              ownsNonprofit={ownsNonprofit}
+            />
+            <TestimonialsBlock
+              nonprofit={nonprofit}
+              ownsNonprofit={ownsNonprofit}
+            />
           </MasonryList>
         </div>
       )}
       {selected === "our impact" && (
         <div className="w-full m-0 p-0 min-w-280px flex flex-col t:flex-row t:flex-wrap items-center gap-29px">
-          <AboutUsBlock nonprofit={nonprofit} ownsNonprofit={ownsNonprofit} displayLocationAndInterests={true} />
+          <AboutUsBlock
+            nonprofit={nonprofit}
+            ownsNonprofit={ownsNonprofit}
+            displayLocationAndInterests={true}
+          />
           <MasonryList>
-            <GalleryBlock nonprofit={nonprofit} ownsNonprofit={ownsNonprofit}/>
-            <TestimonialsBlock nonprofit={nonprofit} ownsNonprofit={ownsNonprofit}/>
+            <GalleryBlock nonprofit={nonprofit} ownsNonprofit={ownsNonprofit} />
+            <TestimonialsBlock
+              nonprofit={nonprofit}
+              ownsNonprofit={ownsNonprofit}
+            />
           </MasonryList>
         </div>
       )}
       {selected === "campaigns" && (
         <div className="w-full m-0 p-0 min-w-280px flex flex-col t:flex-row t:flex-wrap items-center gap-29px">
           <CampaignsBlock nonprofit={nonprofit} ownsNonprofit={ownsNonprofit} />
-          <ClosedCampaignsBlock nonprofit={nonprofit} ownsNonprofit={ownsNonprofit} />
+          <ClosedCampaignsBlock
+            nonprofit={nonprofit}
+            ownsNonprofit={ownsNonprofit}
+          />
         </div>
       )}
       {/*{selected === "fundraiser" && <HomeTab nonprofit={nonprofit} ownsNonprofit={ownsNonprofit}/>}*/}
